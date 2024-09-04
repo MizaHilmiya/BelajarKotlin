fda5ed87a6d1e2874f76155213c0e06ae0e20881
MizaHilmiya <mizahilmiya02@gmail.com>
2024-09-04 13:54:49 +0700

    add MainApp.kt

---
+++ src/main/kotlin/BlokAndComment.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin")
+    //contoh komentar
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * Kotlin doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
---
+++ src/main/kotlin/VarVal.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varVariable: Int
+    varVariable = 56
+    println("Pemberian nilai pertama pada varVaribel = "+varVariable)
+    varVariable = 78
+    println("Pemberian nilai kedua pada varVaribel = "+varVariable)
+    val phi = 3.14
+    //phi = 2.5
+}
