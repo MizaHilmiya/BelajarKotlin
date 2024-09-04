8ea69cf130899e950dbe0b2778039058dab4cb67
MizaHilmiya <mizahilmiya02@gmail.com>
2024-09-04 14:15:43 +0700

    add String.kt

--- src/main/kotlin/String.kt
+++ src/main/kotlin/String.kt
@@ -4,4 +4,21 @@ fun main(args: Array<String>) {
         print(chr)
     }
     print('\n')
+
+    val s = "abc" + 1
+    println(s+ "def")
+
+    /*var  text = """
+        for (c in "foo")
+            print(c) 
+    """
+    println(text)*/
+
+    var text= """
+        |Tell me and I forget.
+        |Teach me and I remember.
+        |Involve me and I learn.
+        |(Benjamin Frenklin)
+    """.trimIndent()
+    println(text)
 }
